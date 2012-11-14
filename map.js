0b9641a04ed3e83c19728f850156b4c5effe9020
V$H. <ashermartin@gmail.com>
2012-11-14 15:02:11 -0600

    Update map.js
    Update to the Locate() and Map() to take in arrays of addresses.  Google Mapping and Marker Details still needs to be updated.

--- map.js
+++ map.js
@@ -1,76 +1,150 @@
-// DATE: 8/2012
-// AUTHOR: V$H 
-
 /*
-Map(address, id) // id isn't needed
-  address = "string" (by default or... you can create a dictionary)
-  address.fullname = "1111 W. 35th Street, Chicago, IL 60609 USA";
-  address.provider = "open"; or "google"; // whatever dataprovider you want to read the lat/lon from.
-  address.mapper = "open"; or "google"; // whatever you want to map your result
-  address.type = "json" or "xml"; // json is default
-  address.id = "my-div"; // location that the image should be placed the name of the ID tag
+       jMAP: JavaScript Geocoder & Mapper (OpenStreetMaps & Google Maps)
+    Version: 0.5 (PRELIMINARY)
+       Date: 11/2012
+
+OPEN STREET MAPS API: http://wiki.openstreetmap.org/wiki/Nominatim#Example (~300 ms per xml/json request - 10/2012)
+GOOGLE MAPS API: https://developers.google.com/maps/documentation/geocoding/#JSON (~100 ms per request)
+
+Example: (id is the element in the HTML DOM where you want the map)
+
+  Map(address, id);
+    UNSUPPORTED -- var address = {lat : 40, lon : -80};
+    var address = "101 W. Maddison St., Chicago, IL";
+    var address = ["101 W. Maddison St., Chicago, IL", "102 W. Maddison St., Chicago, IL"];
+    UNSUPPORTED -- var address = {address : "101 W. Maddison St., Chicago, IL", mapper : "open", provider : "google", type : "json", html : ""};
+    var address = [{address : "101 W. Maddison St., Chicago, IL"},{address : "101 W. Maddison St., Chicago, IL"}];
+    var address = [{lat : 40, lon : -80}, {lat : 41, lon : -81}];
 */
 
-// OPEN STREET MAPS API: http://wiki.openstreetmap.org/wiki/Nominatim#Example
-// GOOGLE MAPS API: https://developers.google.com/maps/documentation/geocoding/#JSON
+/*
 
-// FUTURE: this code should be library independent and rely only on native JavaScript for XML/JSON parsing.
+Map(["Chicago, IL", "San Jose, CA"], "myDivId");
+  Returns a map to the myDivId with two markers
 
-function Map(address, id) { 
-  var uri = "request.php"; // the uri used to do a remote geocode lookup 
-  var type = "xml"; // json or xml request type
-  var addr = ""; // the address string copied from the input address
-  var provider = "google"; // google or open (who you want to make the geo code request from) 
-  var mapper = "open"; // google or open (who you want to display the map)
-  var id = "mapdiv"; // the location div block id tag where you want your map placed
-  var style = {height : "400px", width : "400px", zoom : 15};
+*/
+function Map(address, id){
 
-  if(address.fullname != null){
-    uri = address.uri;
-    type = address.type;
-    type = type.toLowerCase();
-    addr = address.fullname;
-    provider = address.provider; 
-    provider = provider.toLowerCase();
-    mapper = address.mapper; 
-    mapper = mapper.toLowerCase();
-    if(id == null){
-      id = address.id;
-    } else {
-      id = id;
-    }
-    style = address.style;
-  } else {
-    addr = address;
-    if(id == null){
-      var div = document.createElement('div');
-      div.setAttribute('id', id);
-      var element = document.body; // locate the body of the HTML
-      element.appendChild(div); // create a default div block to place the map
-    } else 
-    id = id;
+  var checked = FormatAddress(address);
+  
+  if(checked.locations[0].lat != null){
+    if(checked.mapper == "open"){
+      MapOpen(checked.locations, id, checked.style);
+    } 
+    else { // default to google maps api
+      MapGoogle(checked.locations, id, checked.style);
+    } 
+  } 
+  else { // callback function  
+    Locate(checked.locations, 
+      function (reply) {
+        if(checked.mapper == "open"){
+          MapOpen(reply, id, checked.style);
+        } 
+        else { // default to google maps api
+          MapGoogle(reply, id, checked.style);
+        } 
+      });
   }
-
-  if(provider == "google") {
-    uri = uri+"?uri="+GoogleURI(addr, type)+"";
-  } else { // open street maps is the default ;O)
-    uri = uri+"?uri="+OpenURI(addr, type)+""; 
-  }
-
-  Request(uri, function(reply){
-    var location = {};
-    location = Parser(reply.response, provider, type);
-      if(mapper == "open") {
-        MapOpen(location, id, style);
-      } 
-      else { // default to google maps api
-        MapGoogle(location, id, style);
-      } 
-  });  
 }
 
-// Parser(ajax reply, google/open, json/xml); 
-// takes the reply from google maps or open street maps and creates an object with location[lat/lon] 
+/* 
+
+Locate(["Chicago, IL", "San Jose, CA"], callback);
+  Returns the lat / lon of both of the address to the callback funtion
+
+*/
+function Locate(address, callback) { 
+
+  var checked = FormatAddress(address);
+  
+  var addresses = checked.locations; // the address string copied from the input address
+  var type = checked.type; // json or xml request type (json is slightly faster for both google and open)
+  var provider = checked.provider; // "google" or "open" for open street maps (xml/json geo (lat/lon) code request) 
+
+  // index through all the addresses and put them into an array of request uri's
+  var uri = "request.php"; // please dont change this... the uri used to do a remote geocode lookup   
+  var uris = [];  
+  for(var index = 0; index < addresses.length; index++){
+    if(provider == "google") {
+      uris.push(uri+"?uri="+GoogleURI(addresses[index], type)+"");
+    } 
+    else { // open street maps is the default ;O)
+      uris.push(uri+"?uri="+OpenURI(addresses[index], type)+"");
+    }
+  }
+  // make all the requests for the geo locations (lat/lon)
+  // keep these 3 here for closure reasons... 
+  var locations = []; 
+  var center = {lat : 0, lon : 0};
+  
+  for (var u = 0; u < uris.length; u++) {
+    Request(uris[u], function(reply){
+      var location = {};
+      location = Parser(reply.response, provider, type);
+      locations.push(location);
+      center.lat += location.lat;
+      center.lon += location.lon;
+      if(locations.length == uris.length) {
+        center.lat = center.lat/uris.length;
+        center.lon = center.lon/uris.length;
+        locations.center = center; // the center is the geometric mean = Math.pow(n1 * n2 * n3..., 1/count);
+        callback(locations);
+      }
+    });
+  }   
+}
+
+function FormatAddress(address) {
+
+  var locations = []; // an array of the addresses 
+  
+  var specs = { provider : "open", // "google" or "open" for (lat/lon) geo code request 
+                mapper : "open", // "google" or "open" (who you want to display the map)
+                type : "json", // "json" or "xml" request type from the provider
+                html : "",  // html to display in the info box
+                style : { height : "400px", // map styles
+                          width : "400px", 
+                          zoom : 15}};
+
+  if(typeof address[0] == "object" || address[0].length > 1){
+    locations = address;
+  }
+  else {
+    locations = [address];
+  }
+
+  if(typeof locations[0] == "object"){
+    if(locations[0].type != null){
+      specs.type = locations[0].type.toLowerCase();
+    }
+    
+    if(locations[0].provider != null){
+      specs.provider = locations[0].provider.toLowerCase(); 
+    }
+    
+    if(locations[0].mapper != null) {
+      specs.mapper = locations[0].mapper.toLowerCase();
+    }
+    
+    if(locations[0].style != null) {
+      specs.style = locations[0].style;
+    } 
+  
+    if(locations[0].html != null) {
+      specs.html = locations[0].html;
+    } 
+  }
+
+  return {locations : locations, html : specs.html, type : specs.type, provider : specs.provider, mapper : specs.mapper, style : specs.style};
+  
+}
+
+/* 
+Parser(ajax reply, google/open, json/xml); 
+   takes the reply from google maps or open street maps and creates an object with location[lat/lon] 
+*/
+
 function Parser(text, provider, type) {
   var location = {};
   var geo = {};
@@ -89,26 +163,28 @@ function Parser(text, provider, type) {
     if(provider == "google") { // Google Street Maps
       switch(type) {
         case "xml":
-          location["lat"] = reply.getElementsByTagName("lat")[0].textContent; 
-          location["lon"] = reply.getElementsByTagName("lng")[0].textContent; 
-          break;
+          location["lat"] = reply.getElementsByTagName("lat")[0] ? reply.getElementsByTagName("lat")[0].textContent : null;
+          location["lon"] = reply.getElementsByTagName("lng")[0] ? reply.getElementsByTagName("lng")[0].textContent : null; 
+        break;
         default: // json
-          location["lat"] = reply.results[0].geometry.location.lat;
-          location["lon"] = reply.results[0].geometry.location.lng;
+          location["lat"] = reply.results[0] ? reply.results[0].geometry.location.lat : null;
+          location["lon"] = reply.results[0] ? reply.results[0].geometry.location.lng : null;
       }
     }
     else { // Open Street Maps
-      switch(type) {
-      case "xml":
-        location["lat"] = reply.getElementsByTagName("place")[0].getAttribute("lat"); 
-        location["lon"] = reply.getElementsByTagName("place")[0].getAttribute("lon"); 
+      switch(type) { // TODO BUG CHECK
+        case "xml":
+          location["lat"] = reply.getElementsByTagName("place")[0] ? reply.getElementsByTagName("place")[0].getAttribute("lat") : null; 
+          location["lon"] = reply.getElementsByTagName("place")[0] ? reply.getElementsByTagName("place")[0].getAttribute("lon") : null;
         break;
-      default: // json
-        location["lat"] = reply[0].lat;
-        location["lon"] = reply[0].lon;
+        default: // json / TODO / BUG / CHECK?
+          location["lat"] = reply[0] ? reply[0].lat : null;
+          location["lon"] = reply[0] ? reply[0].lon : null;
       }
     }
   }
+  location.lat = parseFloat(location.lat);
+  location.lon = parseFloat(location.lon);
   return location;
 }
 
@@ -118,32 +194,25 @@ function Parser(text, provider, type) {
 // & 	%26
 // space %20 
 // this version only replaces spaces with %20 because the php request does the rest
-function FormatAddress(address) {
-  var result = "";
-  if(typeof address == "string") {
-    return address.replace(/ /g, "%20");
-  } else { // check the address carefully... this might not always work... 
-    for (addr in address){
-      address[addr] = address[addr].replace(/ /g, "%20");
-      if((addr != "state") && (addr != "zip")){
-        //result += "+" + address[addr] + ",";
-        result += "%20" + address[addr] + "%2C"; // if uri encoding is needed
-      }
-      else {
-        //result += "+" + address[addr];
-        result += "%20" + address[addr]; // if uri encoding is needed
-      }
-    }
-  return result;
+
+function FormatURI(name) {
+  if(typeof name == "string") {
+    return name.replace(/ /g, "%20");
+  } 
+  else { // check the address carefully... this might not always work... 
+    return name.address.replace(/ /g, "%20");
   }
 }
 
 // GOOGLE MAPS API v3
 // API: https://developers.google.com/maps/documentation/geocoding/#GeocodingRequests
 // Example JSON request: http://maps.googleapis.com/maps/api/geocode/json?address=1111%W.%2035rd%20street,%20Chicago,%20IL&sensor=true
+//   JSON requests takes about 70 ms each
+//   XML requests take about 80 ms each
+
 function GoogleURI(address, type){
   var uri = "http://maps.googleapis.com/maps/api/geocode/";
-  address = FormatAddress(address);
+  address = FormatURI(address);
   if(type == "xml"){
     uri = uri + type + "?" + "address=" + address + "%26sensor=false";
   } else { // default to json
@@ -157,11 +226,14 @@ function GoogleURI(address, type){
 // Example XML Request: http://nominatim.openstreetmap.org/search?q=%201111%20W.%2035th%20Street,%20Chicago&format=xml&addressdetails=1
 // NOTE &'s and spaces dont pass easily to php and then to nominatim.openstreetmap.org
 // WARNING: OPEN STREET MAPS SOMETIMES DOESNT NEED THE STATE AND ZIP CODE and in fact will error out... ;)
+// JSON requests take about 359 ms (depends on traceroute!)
+// XML requests takea about 375 ms (depends on traceroute!)
+
 function OpenURI(address, type){
   var uri = "http://nominatim.openstreetmap.org/search?q=";
   var format = "%26format=" + type; 
   var details = "%26addressdetails=1";
-  address = FormatAddress(address);
+  address = FormatURI(address);
   // NOTE: &'s dont pass good to php file_get_contents($uri) dont use "&polygon=1&addressdetails=1";
   if(type == "xml"){
     uri = uri + address + "%26format=" + type + "%26addressdetails=1";
@@ -173,6 +245,7 @@ function OpenURI(address, type){
 
 // Request(request.php, callback handler, data for post requests isn't used yet)
 // really doesn't return anything and needs a callback function handler
+
 function Request(uri, callback, data) { //data = null; // right now we dont support post requests.. :(
   var xmlhttp;
   if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -198,7 +271,7 @@ function Request(uri, callback, data) { //data = null; // right now we dont supp
 
 // location.lat / location.lon 
 // id = <div id="id">
-function MapGoogle(location, id, style) {
+function MapGoogle(locations, id, style) {
 
   if(style == null) {
     style = {};
@@ -206,33 +279,74 @@ function MapGoogle(location, id, style) {
     style.width = "300px";
     style.zoom = 15;
   }
-
-  var latlng = new google.maps.LatLng(location.lat, location.lon);
+  
+  // get the center of the map / default to locations[0]
+  // TODO: add locations.center?
+  if(locations.center){
+    locations.center.lat = locations.center.lat;
+    locations.center.lon = locations.center.lon;
+  } 
+  else {
+    locations.center = {};
+    locations.center.lat = locations[0].lat;
+    locations.center.lon = locations[0].lon;
+  }
+  
+  var center = new google.maps.LatLng(locations.center.lat, locations.center.lon);
   var options = { zoom: style.zoom,
-                  center: latlng,
+                  center: center,
                   mapTypeId: google.maps.MapTypeId.ROADMAP}
 
   var element = document.getElementById(id);
   element.style = style;
   var map = new google.maps.Map(element, options);
- 
-  var marker = new google.maps.Marker({
-          map:map,
-          draggable:true,
-          animation: google.maps.Animation.DROP,
-          position: latlng
-  });
-    
-  google.maps.event.addListener(marker, 'click', function() {
-    if (marker.getAnimation() != null) { marker.setAnimation(null); } 
-    else { marker.setAnimation(google.maps.Animation.BOUNCE);}
-  });
+  GoogleMarkers(locations, map);
+  return map;
   
 }
 
-// location.lat / location.lon 
+// Adding Google Markers
+// https://developers.google.com/maps/documentation/javascript/overlays#Markers
+function GoogleMarkers(locations, map) {
+
+  // TODO: add labels
+  var labels = locations.labels != null ? locations.labels : false;
+  var label = {};
+  var markers = [];
+  var marker;
+  
+  for (var index = 0; index < locations.length; index++) {
+    if(!(isNaN(locations[index].lon) || isNaN(locations[index].lat))) {
+      var geo = new google.maps.LatLng(locations[index].lat, locations[index].lon);
+      if(labels) {
+        label.title = labels[index].title;
+        label.index = labels[index].index;
+      } 
+      else {
+        label.title = "";
+        label.index = "";    
+      }
+      
+      marker = new google.maps.Marker({
+        position : geo,
+        map : map,
+        //draggable : true,
+        //shadow : shadow,
+        //icon : image,
+        //shape : shape,
+        //title : label.title,
+        //zIndex : label.index
+      });
+      //markers.push(marker);
+    }
+    //return markers;
+  }  
+}
+
+// locations = [{lat : 0.00 lon : 0.00}, {}, {}] 
 // id = <div id="id">
-function MapOpen(location, id, style) {
+// http://wiki.openstreetmap.org/wiki/OpenLayers_Simple_Example
+function MapOpen(locations, id, style) {
 
   if(style == null) {
     style = {}; 
@@ -240,37 +354,61 @@ function MapOpen(location, id, style) {
     style.width = "300px";
     style.zoom = 5;
   }
-
-  element = document.getElementById(id);
-  element.style.width=style.width;
-  element.style.height=style.height;
   
-  if(document.getElementById(id).childNodes()){
-        
-  }
-  map = new OpenLayers.Map(id);
-  map.addLayer(new OpenLayers.Layer.OSM());   // TODO: placement needed
- 
-  var lon = location.lon; 
-  var lat = location.lat; 
- 
-  var lonLat = new OpenLayers.LonLat( lon, lat )
-        .transform(
-          new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-          map.getProjectionObject() // to Spherical Mercator Projection
-        );
+  var map = new OpenLayers.Map(id);
+  var mapnik = new OpenLayers.Layer.OSM(); // if you want to show that its an OpenStreetMap
+  map.addLayer(mapnik);
 
-  var markers = new OpenLayers.Layer.Markers( "Markers" );
+  var markers = new OpenLayers.Layer.Markers("Markers");
   map.addLayer(markers);
+  var marks = OpenMarkers(locations, markers)
+  map.setCenter(marks.center, style.zoom);
+  
+}
 
-  markers.addMarker(new OpenLayers.Marker(lonLat));
+// OpenMarkers([{lat : 0.00, lon : 0.00},{},...]);
+// returns a lit of all the markers to be added in Open Street Maps Geo Code Format
+//    and also something that is approximatly the center of the markers... (speed?)
+function OpenMarkers(locations, markers) {
 
-  map.setCenter (lonLat, style.zoom);
+  //var markers = new OpenLayers.Layer.Markers("Markers");
+  var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+  var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+  var position;
+
+  var position; 
+  var lon;
+  var lat;
+  var average = {lon : 0.0, lat : 0.0};
+  var total = locations.length;
+  
+  for (var loc = 0; loc < locations.length; loc++) {
+    lon = locations[loc].lon; 
+    lat = locations[loc].lat;
+    if(!(isNaN(lon) || isNaN(lat))) {
+      average.lon += lon;
+      average.lat += lat;
+      position = new OpenLayers.LonLat(lon, lat).transform( fromProjection, toProjection);
+      marker = new OpenLayers.Marker(position)
+      markers.addMarker(marker);
+    }
+  }
+  
+  // next calculate the center of the markers... this is approximately the center
+  average.lon = average.lon / total;
+  average.lat = average.lat / total;
+  
+  // TODO: Add harmonic mean to center the map.
+  var approximate = new OpenLayers.LonLat(average.lon, average.lat).transform( fromProjection, toProjection);
+
+  return {markers : markers, center : approximate};
+
 }
 
 // convert the an XML text string to an XML object
 // window.JSON.parse (native json parser)
-var XML = {parse : function(text){
+var XML = {parse : 
+  function(text){
     var obj = {};
     if (window.ActiveXObject){ // TODO: not tested on IE (is this even needed anymore??)
       obj=new ActiveXObject('Microsoft.XMLDOM');
